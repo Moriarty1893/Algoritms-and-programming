fdbce6917d03bb3113e34674f1e7ec536157ccc0
Moriarty1893 <93351713+Moriarty1893@users.noreply.github.com>
2023-03-31 21:57:09 +0300

    Add files via upload

---
+++ _Курсовая_Длл/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Курсовая_Длл
+{
+    public class Class1
+    {
+    }
+}
